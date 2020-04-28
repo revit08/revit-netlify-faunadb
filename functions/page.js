a349279b33cb8c876c325689587732c83fc93e8b
Ganesan Karuppaiya <ganesan.intech@gmail.com>
2020-04-28 16:07:55 +0530

    Create page.js

---
+++ functions/page.js
@@ -0,0 +1,37 @@
+/* Import faunaDB sdk */
+const faunadb = require("faunadb");
+const getId = require("./utils/getId");
+const q = faunadb.query;
+
+exports.handler = (event, context) => {
+  /* configure faunaDB Client with our secret */
+  const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET,
+  });
+  const id = getId(event.path);
+  console.log(`Function 'todo-read' invoked. Read id: ${id}`);
+  return client
+    .query(q.Get(q.Ref(`classes/contentPages/${id}`)))
+    .then((response) => {
+      console.log("success", response);
+      return {
+        statusCode: 200,
+        headers: {
+          "content-type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(response),
+      };
+    })
+    .catch((error) => {
+      console.log("error", error);
+      return {
+        statusCode: 400,
+        headers: {
+          "content-type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(error),
+      };
+    });
+};
