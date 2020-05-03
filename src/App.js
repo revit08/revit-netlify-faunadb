5f457d6287b6107d886d4a0bbe911c2f4bd3160a
Ganesan Karuppaiya <ganesan.intech@gmail.com>
2020-05-03 14:07:19 +0530

    update identiyt

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import netlifyIdentity from "netlify-identity-widget";
 import "./App.css";
 import AdminFooter from "./components/Footers/AdminFooter.js";
 import Header from "./components/Header/Header";
@@ -15,18 +16,54 @@ import Session from "./views/Session";
 
 import routes from "./routes.js";
 export default class App extends Component {
-  state = {
-    showMenu: false,
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null,
+    };
+    netlifyIdentity.init();
+  }
   componentDidMount() {
     // Fetch all todos
+    const user = netlifyIdentity.currentUser();
+    console.log({ user });
+    if (user) this.setState({ user });
   }
+  login = () => {
+    netlifyIdentity.ctx = this;
+    console.log("loging...");
+    netlifyIdentity.open();
+    netlifyIdentity.on("login", (user) => {
+      console.log("user got!", user);
+      netlifyIdentity.ctx.setState({ user });
+      netlifyIdentity.close();
+      //setTimeout(() => navigate("/"), 400);
+    });
+  };
 
+  logout = () => {
+    netlifyIdentity.ctx = this;
+    console.log("logout...");
+    netlifyIdentity.logout();
+    netlifyIdentity.on("logout", () => {
+      console.log("user logout!");
+      netlifyIdentity.ctx.setState({ user: null });
+    });
+  };
   render() {
     return (
       <div class="bg-light">
         <BrowserRouter>
           <Header {...this.props} routes={routes} />
+          {this.state.user ? (
+            <div>
+              <p>Welcome: {this.state.user.user_metadata.full_name}</p>
+              <button onClick={this.logout}>Log out</button>
+            </div>
+          ) : (
+            <button onClick={this.login}>Log in</button>
+          )}
           <div className="main-content">
             <Switch>
               <Route exact path="/" component={Home} />
