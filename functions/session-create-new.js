1901f5adde84f78f5248a18435b982626234c634
Ganesan Karuppaiya <ganesan.intech@gmail.com>
2020-05-07 18:26:49 +0530

    update session create

--- functions/session-create-new.js
+++ functions/session-create-new.js
@@ -1,34 +1,31 @@
-import faunadb from 'faunadb' /* Import faunaDB sdk */
-
-/* configure faunaDB Client with our secret */
+/* Import faunaDB sdk */
+const faunadb = require('faunadb')
 const q = faunadb.query
-const client = new faunadb.Client({
-  secret: process.env.FAUNADB_SERVER_SECRET
-})
 
 /* export our lambda function as named "handler" export */
-exports.handler = (session, context, callback) => {
+exports.handler = async (event, context) => {
+  /* configure faunaDB Client with our secret */
+  const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET
+  })  
   /* parse the string body into a useable JS object */
-  const data = JSON.parse(session.body)
-  // console.log('Function `session-create` invoked', data)
-  const sessionItem = {
+  const data = JSON.parse(event.body)
+  const todoItem = {
     data: data
   }
   /* construct the fauna query */
-  return client.query(q.Create(q.Ref('classes/session'), sessionItem))
+  return client.query(q.Create(q.Ref('classes/session'), todoItem))
     .then((response) => {
-      // console.log('success', response)
       /* Success! return the response with statusCode 200 */
-      return callback(null, {
+      return {
         statusCode: 200,
         body: JSON.stringify(response)
-      })
+      }
     }).catch((error) => {
-      // console.log('error', error)
       /* Error! return the error with statusCode 400 */
-      return callback(null, {
+      return {
         statusCode: 400,
         body: JSON.stringify(error)
-      })
+      }
     })
 }
